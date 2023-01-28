59667af878b7f36b11f9a0ef3e7d352312c6b85e
Anna199421 <111239829+Anna199421@users.noreply.github.com>
2023-01-28 16:14:51 +0200

    irit

---
+++ src/js/01-gallery.js
@@ -0,0 +1,24 @@
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items';
+// Change code below this line
+const container = document.querySelector('.gallery');
+container.insertAdjacentHTML('beforeend', markingUp(galleryItems));
+new SimpleLightbox('.gallery a');
+
+function markingUp(gallery) {
+    return gallery.reduce((acc, { preview, original, description }) => {
+      acc += `<a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>`;
+      return acc;
+    }, '');
+  }
+console.log(galleryItems);
+
