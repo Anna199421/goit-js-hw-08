c8101f2d221feeec1cf36c024a279de19230960d
Anna199421 <111239829+Anna199421@users.noreply.github.com>
2023-01-29 15:40:29 +0200

    iri

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+playerSetCurrentTime();
+
+function playerSetCurrentTime() {
+  if (localStorage.getItem(LOCALSTORAGE_KEY)) {
+    player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));
+  }
+}
+
+function onPlay(data) {
+  localStorage.setItem(LOCALSTORAGE_KEY, data.seconds);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const inputEl = form.querySelector('input');
+const textAreaEl = form.querySelector('textarea');
+
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+let data = {};
+if (localStorage.getItem(LOCALSTORAGE_KEY)) {
+  data = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+}
+
+document.addEventListener('DOMContentLoaded', checkingLocaleStorage);
+form.addEventListener('input', throttle(localStorageUpdate, 500));
+form.addEventListener('submit', retrievingData);
+
+function localStorageUpdate({ target }) {
+  data[target.name] = target.value;
+  // if (target.getAttribute('type') === 'email') {
+  //   data.email = target.value;
+  // }
+  // if (target.getAttribute('name') === 'message') {
+  //   data.message = target.value;
+  // }
+
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(data));
+}
+
+function checkingLocaleStorage() {
+  if (localStorage.getItem(LOCALSTORAGE_KEY)) {
+    const localStorageData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+    inputEl.value = localStorageData.email || '';
+    textAreaEl.value = localStorageData.message || '';
+  }
+}
+
+function retrievingData(e) {
+  e.preventDefault();
+  const { email, message } = e.target.elements;
+  data.email = email.value;
+  data.message = message.value;
+  form.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+  console.log(data);
+}
